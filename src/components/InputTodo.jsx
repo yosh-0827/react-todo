411f95278b2f4e057d52933d7ad1c266f6facfbd
Yosh-0827 <yuyuyu.1648@gmail.com>
2023-11-11 18:44:38 +0900

    入力エリアCSSをコンポーネントに入れる

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "30px",
+  borderRadius: "8px",
+  padding: "8px",
+  margin: "8px",
+};
+
 /**
  *入力エリアのコンポーネント
  * @param {} props
@@ -8,7 +17,7 @@ import React from "react";
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
       <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
       <button onClick={onClick}>追加</button>
     </div>
